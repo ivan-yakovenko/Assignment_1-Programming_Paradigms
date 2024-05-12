4abc9757586a20298f0697d5f2039e37cd6604ce
Ivan Yakovenko <ivan2005y9@gmail.com>
2024-05-12 16:23:50 +0300

    added some changes

--- Assignment_1/DynamicArray.h
+++ Assignment_1/DynamicArray.h
@@ -3,17 +3,23 @@
 #include <string.h>
 
 struct DynamicArray {
-    int* data;
+    char** data;
     size_t size;
     size_t capacity;
 
 };
 
-void DynamicArray_init(struct DynamicArray* dynamicArray) {
-    dynamicArray->data = nullptr;
+void DynamicArrayDefault(struct DynamicArray* dynamicArray) {
+    dynamicArray->data = malloc(dynamicArray->capacity);
     dynamicArray->size = 0;
     dynamicArray->capacity = 1;
 }
+
+//void DynamicArray(struct DynamicArray* dynamicArray, int capacity) {
+//    dynamicArray->capacity = capacity;
+//    dynamicArray->data = malloc(dynamicArray->capacity);
+//    dynamicArray->size = 0;
+//}
 
 size_t getSize(struct DynamicArray* dynamicArray) {
     return dynamicArray->size;
@@ -25,23 +31,21 @@ size_t getCapacity(struct DynamicArray* dynamicArray) {
 
 void Resize(struct DynamicArray* dynamicArray) {
     size_t newCapacity = dynamicArray->capacity * 2;
-    int* newData = (int*)realloc(dynamicArray->data,newCapacity*sizeof(int));
+    char** newData = (char**)realloc(dynamicArray->data,newCapacity*sizeof(char*));
 
     dynamicArray->data = newData;
     dynamicArray->capacity = newCapacity;
 }
 
-void PushBack(struct DynamicArray* dynamicArray, int newValue) {
+void PushBack(struct DynamicArray* dynamicArray, char* newValue) {
     if(dynamicArray->size == dynamicArray->capacity) {
         Resize(dynamicArray);
     }
     dynamicArray->data[dynamicArray->size++] = newValue;
 }
 
-
-
-
-
-
-
-
+void Print(struct DynamicArray* dynamicArray) {
+    for(int i = 0; i < dynamicArray->size; i++) {
+        printf("%s\n", dynamicArray->data[i]);
+    }
+}
